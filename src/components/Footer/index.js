849d8eb318dfa8ec33bbb5681369c1ed20a9d931
Brett herrell <brett_herrell@yahoo.com>
2021-12-29 16:06:36 -0600

    made a sticky footer with some links that will be updated in the future

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <footer className="footer">
+      <nav className="footer-nav">
+        <ul className="footer-list">
+          <li>
+            <a href="https://github.com/Juiceke">Github</a>
+          </li>
+          <li>Resume</li>
+          <li>
+            {/* add a professional twitter link later */}
+            <a href="https://twitter.com">Twitter</a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
