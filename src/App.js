c91bce69e8c62a03f34c5943e015693f8314affb
Brett herrell <brett_herrell@yahoo.com>
2022-01-02 00:52:29 -0600

    added a header with some test info for now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+// import the About section
+// import the Contact section
+// import the Resume section
 
 function App() {
+  const [sections] = useState([
+    {
+      name: "Test",
+      description: "This is a test description!",
+    },
+  ]);
+
+  const [currentSection, setCurrentSection] = useState(sections[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
-      <Header></Header>
-      <div className="content">yo{/* Actual content */}</div>
+      <Header
+        sections={sections}
+        setCurrentSection={setCurrentSection}
+        currentSection={currentSection}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Header>
+      <main>yo{/* Actual content */}</main>
       <Footer></Footer>
     </div>
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function Header() {
+function Header(props) {
+  const {
+    sections = [],
+    setCurrentSection,
+    contactSelected,
+    currentSection,
+    setContactSelected,
+  } = props;
+
+  useEffect(() => {
+    document.title = currentSection.name;
+  }, [currentSection]);
+
   return (
-    <header>
-      <h1>This is a test</h1>
+    <header className="header">
+      <h1 className="name">David Herrell</h1>
+      <nav className="header-nav">
+        <ul className="header-list">
+          <li id="about">About Me</li>
+          <li id="portfolio">Portfolio</li>
+          {sections.map((sections) => (
+            <li
+              className={`${currentSection.name === sections.name}`}
+              key={sections.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentSection(sections);
+                  setContactSelected(false);
+                }}
+              >
+                {sections.name}
+              </span>
+            </li>
+          ))}
+          <li id="contact">Contact</li>
+        </ul>
+      </nav>
     </header>
   );
 }
